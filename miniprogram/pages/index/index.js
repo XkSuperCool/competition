988696629edaaa848a87971d5a7a63865309c68c
XkSuperCool <1175047471@qq.com>
2021-03-24 22:51:21 +0800

    feat: 增加小程序首页样式

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,3 +1,18 @@
 Page({
-
+  data: {
+    banners: [
+      {
+        id: 1,
+        url: 'https://picsum.photos/300/200?random=1',
+      },
+      {
+        id: 2,
+        url: 'https://picsum.photos/300/200?random=2',
+      },
+      {
+        id: 3,
+        url: 'https://picsum.photos/300/200?random=3',
+      },
+    ],
+  },
 });
