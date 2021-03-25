b44aadc14967da84a47f0187c7b5acfe1166988d
= <=>
2021-03-25 17:54:26 +0800

    feat: 小程序地理定位

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,8 @@
+import { isOpenAuthority, launchAuthorize } from '../../utils/util';
+
 Page({
   data: {
+    location: '定位中',
     banners: [
       {
         id: 1,
@@ -15,4 +18,52 @@ Page({
       },
     ],
   },
+  onReady() {
+    this.locationAuth();
+  },
+  // 处理用户点击位置操作
+  async handleLocation() {
+    const isOpenUserLocation = await isOpenAuthority('userLocation');
+    if (!isOpenUserLocation) {
+      wx.openSetting({
+        withSubscriptions: true,
+        success: (res) => {
+          if (res.authSetting['scope.userLocation']) {
+            this.getLocation();
+          }
+        }
+      });
+    }
+  },
+  // 获取用户位置
+  getLocation() {
+    wx.getLocation({
+      success: ({ latitude, longitude }) => {
+        wx.request({
+          url: 'https://apis.map.qq.com/ws/geocoder/v1/?location=' + latitude + ',' + longitude + '&',
+          success: (res) => {
+            console.log(res);
+            this.setData({
+              location: '济南',
+            });
+          }
+        })
+      },
+    });
+  },
+  // 位置权限判断
+  async locationAuth() {
+    const auth = 'userLocation';
+    let isOpenUserLocation = await isOpenAuthority(auth);
+    if (!isOpenUserLocation) {
+      isOpenUserLocation = await launchAuthorize(auth);
+    }
+    if (isOpenUserLocation) {
+      this.getLocation();
+    } else {
+      this.setData({
+        location: '获取定位',
+      });
+    }
+  }
 });
--- miniprogram/utils/util.js
+++ miniprogram/utils/util.js
@@ -14,6 +14,48 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+/**
+ * 用户权限校验，判断是否开启了指定权限
+ * @param {string} name 
+ */
+const isOpenAuthority = name => {
+  return new Promise((resolve) => {
+    wx.getSetting({
+      withSubscriptions: true,
+      success: (setting) => {
+        if (setting.authSetting[`scope.${name}`]) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      },
+      fail(err) {
+        resolve(false);
+      },
+    });
+  });
+}
+
+/**
+ * 对指定权限，向用户发起授权
+ * @param {string} name 
+ */
+const launchAuthorize = name => {
+  return new Promise((resolve) => {
+    wx.authorize({
+      scope: 'scope.userLocation',
+      success () {
+        resolve(true);
+      },
+      fail() {
+        resolve(false);
+      }
+    })
+  });
+}
+
 module.exports = {
-  formatTime
+  formatTime,
+  isOpenAuthority,
+  launchAuthorize
 }
